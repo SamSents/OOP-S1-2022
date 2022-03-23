6d0fabe2c96eda6c65c395154093bcac6a1640b6
SamSents <100640889+SamSents@users.noreply.github.com>
2022-03-23 22:23:29 +1030

    Add files via upload

---
+++ function-2-1.cpp
@@ -0,0 +1,37 @@
+//
+//  function-2-1.cpp
+//  
+//
+//  Created by Sam Sentschuk on 23/3/22.
+//
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+void print_sevens(int *nums,int length){
+
+int * addy[length];
+    int Sev[length];
+
+
+for (int i =0 ; i< length; i++) {
+    addy[i] = &nums[i];
+    Sev[i] = *addy[i];
+}
+
+    for (int i =0 ; i< length; i++) {
+        if (Sev[i]%7==0) {
+            cout<<Sev[i];
+        }
+        }
+    
+   
+
+    cout<< endl;
+
+
+
+return;
+}
---
+++ main-2-1.cpp
@@ -0,0 +1,32 @@
+//
+//  main-2-1.cpp
+//  
+//
+//  Created by Sam Sentschuk on 23/3/22.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+extern void print_sevens(int *nums,int length) ;
+
+int main() {
+    
+    
+    int nums[3] = {1,21,7};
+    
+    int length = 3;
+    
+    
+    
+    print_sevens(nums, length);
+    
+    
+    
+    
+    
+    
+    
+    return 1;
+}
