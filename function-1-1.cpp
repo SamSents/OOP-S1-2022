a1dd6ff12b08544f5550334c0849af998baf926c
SamSents <100640889+SamSents@users.noreply.github.com>
2022-03-23 10:44:24 +1030

    Update function-1-1.cpp

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -15,7 +15,7 @@ void copy_2d_strings(std::string first[][2], std::string second[][2], int n)
     
     for (int i =0; i< n; i++) {
         for (int j =0 ; j<3; j++) {
-            first[i][j] = second[i][j];
+            second[i][j] = first[i][j];
         }
     }
     
