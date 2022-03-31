8e8c31965908b307e64bf9714b6ced70de5509b5
SamSents <100640889+SamSents@users.noreply.github.com>
2022-03-31 14:35:45 +1030

    Add files via upload

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -14,9 +14,16 @@ extern string get_phase(int temp);
 
 int main(){
     
-    int temp = 25683;
+    int temp =10;
     cout << "phase at " << temp << "K is: " << get_phase(temp) << endl;
-    
+    temp2 =273;
+    cout << "phase at " << temp << "K is: " << get_phase(temp2) << endl;
+    temp3 =293;
+    cout << "phase at " << temp << "K is: " << get_phase(temp3) << endl;
+    temp4 =4700;
+    cout << "phase at " << temp << "K is: " << get_phase(temp4) << endl;
+    temp5 =5000;
+    cout << "phase at " << temp << "K is: " << get_phase(temp5) << endl;
     
     return 0;
     
